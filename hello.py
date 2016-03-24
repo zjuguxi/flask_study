85b56c9f386226773c867fbe9e36fb79c27e1d24
Alien <zjuguxi@gmail.com>
2016-03-24 23:41:10 +0800

    nothing big

--- hello.py
+++ hello.py
@@ -1,15 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for
 from flask.ext.script import Manager
 from flask.ext.bootstrap import Bootstrap
 from flask.ext.moment import Moment
+from flask.ext.wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
+from datetime import datetime
 
 app = Flask(__name__)
+app.config[ 'SECRET_KEY' ] = 'hard to guess string'
 
 manager = Manager(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
-from datetime import datetime
+class NameForm(Form):
+    name = StringField('what\'s your name?', validators = [Required()] )
+    submit = SubmitField('Submit')
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -17,11 +24,15 @@ def page_not_found(e):
 
 @app.errorhandler(500)
 def internal_server_error(e):
-    return render_templates('500.html'), 500
+    return render_template('500.html'), 500
 
-@app.route('/')
+@app.route('/', method = ['GET', 'POST'])
 def index():
-    return render_template('index.html', current_time = datetime.utcnow())
+    form = NameForm()
+    if from.validate_on_submit():
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html', form = form, name = session.get('name'))
 
 
 @app.route('/user/<name>')
