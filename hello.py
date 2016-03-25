57f85b6015bcf75d99aa51956be65831867a5c95
Alien <zjuguxi@gmail.com>
2016-03-25 15:08:43 +0800

    debugging

--- hello.py
+++ hello.py
@@ -26,21 +26,16 @@ def page_not_found(e):
 def internal_server_error(e):
     return render_template('500.html'), 500
 
-@app.route('/', method = ['GET', 'POST'])
+@app.route('/', methods = ['GET', 'POST'])
 def index():
     form = NameForm()
-    if from.validate_on_submit():
+    if form.validate_on_submit():
         old_name = session.get('name')
         if old_name is not None and old_name != form.name.data:
             flash('Looks like you have changed your name!' )
         session['name'] = form.name.data
         return redirect(url_for('index'))
     return render_template('index.html', form = form, name = session.get('name'))
-
-
-@app.route('/user/<name>')
-def user(name):
-    return render_template('user.html', name=name)
 
 
 if __name__ == '__main__':
