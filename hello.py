8b6122ffdd0080f8a65a04821eb9bd62f6993f1c
Alien <zjuguxi@gmail.com>
2016-04-08 14:57:52 +0800

    excise

--- hello.py
+++ hello.py
@@ -1,11 +1,14 @@
-from flask import Flask
+from flask import Flask, request, make_response, redirect, abort
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return '<h1>Hello World!</h1>'
+    return redirect('http://www.baidu.com')
 
 @app.route('/user/<name>')
 def user(name):
     return '<h1>Hello, %s!</h1>' % name
+
+if __name__ == '__main__':
+    app.run(debug=True)
