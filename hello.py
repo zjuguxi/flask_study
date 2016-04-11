c86d21d4858ff079aa0769d985180212cb3eb59f
Alien <zjuguxi@gmail.com>
2016-04-11 17:47:34 +0800

    template

--- hello.py
+++ hello.py
@@ -1,25 +1,20 @@
-from flask import Flask, request, make_response, redirect, abort
+from flask import Flask, request, make_response, redirect, abort, render_template
 from flask.ext.script import Manager
+from flask.ext.bootstrap import Bootstrap
+
 
 app = Flask(__name__)
 manager = Manager(app)
 
-@app.route('/')
+bootstrap = Bootstrap(app)
+
+@app.route('/index')
 def index():
-    response = make_response('<h1>This document carries a cookie!</h1>')
-    response.set_cookie('answer', '42')
-    return response
+    return render_template('index.html')
 
 @app.route('/user/<name>')
 def user(name):
-    return '<h1>Hello, %s!</h1>' % name
-
-@app.route('/user/<id>')
-def get_user(id):
-    user = load_user(id)
-    if not user:
-        abort(404)
-    return '<h1>Hello, %s</h1>' % user.name
+    return render_template('user.html', name = name)
 
 
 if __name__ == '__main__':
