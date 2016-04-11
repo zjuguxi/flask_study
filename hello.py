cc350f0f7d812da3cec34f705bc4444c95f72b42
Alien <zjuguxi@gmail.com>
2016-04-11 12:22:05 +0800

    edited

--- hello.py
+++ hello.py
@@ -1,14 +1,26 @@
 from flask import Flask, request, make_response, redirect, abort
+from flask.ext.script import Manager
 
 app = Flask(__name__)
+manager = Manager(app)
 
 @app.route('/')
 def index():
-    return redirect('http://www.baidu.com')
+    response = make_response('<h1>This document carries a cookie!</h1>')
+    response.set_cookie('answer', '42')
+    return response
 
 @app.route('/user/<name>')
 def user(name):
     return '<h1>Hello, %s!</h1>' % name
 
+@app.route('/user/<id>')
+def get_user(id):
+    user = load_user(id)
+    if not user:
+        abort(404)
+    return '<h1>Hello, %s</h1>' % user.name
+
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    manager.run()
