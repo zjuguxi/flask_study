e51455867530273aae001b541741a425ab45151e
Alien <zjuguxi@gmail.com>
2016-04-14 16:07:40 +0800

    added moment.js

--- hello.py
+++ hello.py
@@ -1,16 +1,19 @@
 from flask import Flask, request, make_response, redirect, abort, render_template
 from flask.ext.script import Manager
 from flask.ext.bootstrap import Bootstrap
-
+from flask.ext.moment import Moment
+from datetime import datetime
 
 app = Flask(__name__)
 manager = Manager(app)
 
 bootstrap = Bootstrap(app)
 
-@app.route('/index')
+moment = Moment(app)
+
+@app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', current_time = datetiem.utcnow())
 
 @app.route('/user/<name>')
 def user(name):
