b1a2edb167fd2409d7eae278160735dc58d1ad34
Alien <zjuguxi@gmail.com>
2016-04-14 16:32:06 +0800

    edited index.html

--- hello.py
+++ hello.py
@@ -5,15 +5,20 @@ from flask.ext.moment import Moment
 from datetime import datetime
 
 app = Flask(__name__)
+
 manager = Manager(app)
-
 bootstrap = Bootstrap(app)
-
 moment = Moment(app)
-
+'''
 @app.route('/')
 def index():
     return render_template('index.html', current_time = datetiem.utcnow())
+'''
+
+@app.route('/')
+def index():
+    return render_template('index.html',
+                           current_time=datetime.utcnow())
 
 @app.route('/user/<name>')
 def user(name):
