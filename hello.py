cc6cee1ec31e95e964667aeed64c7057cc7061f2
Alien <zjuguxi@gmail.com>
2016-04-18 16:48:06 +0800

    debug

--- hello.py
+++ hello.py
@@ -34,7 +34,7 @@ def index():
     form = NameForm()
     if form.validate_on_submit():
         old_name = session.get('name')
-        if old_name is not None and old_name != form.name.data
+        if old_name is not None and old_name != form.name.data:
             flash('Looks like you have changed your name!')
         session['name'] = form.name.data
         form.name.data = ''
