c34c41641b822b62779dba3a6dca7f1a19f978be
Alien <zjuguxi@gmail.com>
2016-04-18 15:39:49 +0800

    added something

--- hello.py
+++ hello.py
@@ -1,9 +1,8 @@
-from flask import Flask, request, make_response, redirect, abort, render_template
+from flask import Flask, render_template
 from flask.ext.script import Manager
 from flask.ext.bootstrap import Bootstrap
 from flask.ext.moment import Moment
-from datetime import datetime
-from flask.ext.wtf import from
+from flask.ext.wtf import Form
 from wtforms import StringField, SubmitField
 from wtforms.validators import Required
 
@@ -14,9 +13,20 @@ manager = Manager(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
+
 class NameForm(Form):
     name = StringField('What\'s your name?', validators = [Required()])
     submit = SubmitField('Submit')
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
 
 @app.route('/', methods = ['GET', 'POST'])
 def index():
@@ -24,20 +34,8 @@ def index():
     form = NameForm()
     if form.validate_on_submit():
         name = form.name.data
-        form.name.data = ' '
+        form.name.data = ''
     return render_template('index.html', form = form, name = name)
-
-@app.route('/user/<name>')
-def user(name):
-    return render_template('user.html', name = name)
-
-@app.errorhandler(404)
-def page_not_found(e):
-    return render_template('404.html'), 404
-
-@app.errorhandler(500)
-def internal_server_error(e):
-    return render_template('500.html'), 500
 
 if __name__ == '__main__':
     manager.run()
