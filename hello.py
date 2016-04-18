8d8d8c6efcc12ba656c17b1b9f6a825e6208decd
Alien <zjuguxi@gmail.com>
2016-04-18 16:13:41 +0800

    chapter4

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for
 from flask.ext.script import Manager
 from flask.ext.bootstrap import Bootstrap
 from flask.ext.moment import Moment
@@ -28,14 +28,15 @@ def page_not_found(e):
 def internal_server_error(e):
     return render_template('500.html'), 500
 
+
 @app.route('/', methods = ['GET', 'POST'])
 def index():
-    name = None
     form = NameForm()
     if form.validate_on_submit():
-        name = form.name.data
-        form.name.data = ''
-    return render_template('index.html', form = form, name = name)
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html', form = form, name = session.get('name')
+
 
 if __name__ == '__main__':
     manager.run()
