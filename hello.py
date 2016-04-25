7a1c1f99aa269827f32243655cd49e0d5739cf57
Alien <zjuguxi@gmail.com>
2016-04-25 15:30:13 +0800

    add db

--- hello.py
+++ hello.py
@@ -26,17 +26,18 @@ class Role(db.Model):
     __tablename__ = 'roles'
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String(64), unique = True)
-
     def __repr__(self):
         return '<Role %r>' % self.name
+    users = db.relationship('User', backref = 'role')
+
 
 class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String(64), unique = True, index = True)
-
     def __repr__(self):
         return '<User %r>' % self.username
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
 
 
 class NameForm(Form):
