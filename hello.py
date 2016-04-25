6c37da65d4f88807c2e35706dcf9c7e6bd3ef1f9
Alien <zjuguxi@gmail.com>
2016-04-25 17:17:44 +0800

    modified

--- hello.py
+++ hello.py
@@ -6,6 +6,7 @@ from flask.ext.wtf import Form
 from wtforms import StringField, SubmitField
 from wtforms.validators import Required
 from flask.ext.sqlalchemy import SQLAlchemy
+import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
@@ -59,13 +60,18 @@ def internal_server_error(e):
 def index():
     form = NameForm()
     if form.validate_on_submit():
-        old_name = session.get('name')
-        if old_name is not None and old_name != form.name.data:
-            flash('Looks like you have changed your name!')
+        user = User.query.filter_by(username = form.name.data).first()
+        if user is None:
+            user = User(username = form.name.data)
+            db.session.add(user)
+            session['known'] = False
+        else:
+            session['known'] = True
+
         session['name'] = form.name.data
         form.name.data = ''
         return redirect(url_for('index'))
-    return render_template('index.html', form=form, name=session.get('name'))
+    return render_template('index.html', form=form, name=session.get('name'), known = session.get('known', False))
 
 
 if __name__ == '__main__':
