a9efcdcf51c1d73a06bc61e10c27fa17bc819ec6
Alien <zjuguxi@gmail.com>
2016-04-26 16:03:47 +0800

    fix mail

--- hello.py
+++ hello.py
@@ -27,7 +27,7 @@ migrate = Migrate(app, db)
 manager.add_command('db', MigrateCommand)
 mail = Mail(app)
 
-app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
