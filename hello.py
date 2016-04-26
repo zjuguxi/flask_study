127ebbd7f279ce86a54b9da9ee47e15e01a62db9
Alien <zjuguxi@gmail.com>
2016-04-26 17:09:23 +0800

    mail

--- hello.py
+++ hello.py
@@ -1,40 +1,36 @@
-from flask import Flask, render_template, session, redirect, url_for, flash
+import os
+from flask import Flask, render_template, session, redirect, url_for
 from flask.ext.script import Manager, Shell
 from flask.ext.bootstrap import Bootstrap
 from flask.ext.moment import Moment
 from flask.ext.wtf import Form
-from flask.ext.mail import Mail, Message
 from wtforms import StringField, SubmitField
 from wtforms.validators import Required
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.migrate import Migrate, MigrateCommand
-from threading import Thread
-import os
+from flask.ext.mail import Mail, Message
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hard to guess string'
 app.config['SQLALCHEMY_DATABASE_URI'] = \
-'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+    'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
-app.config.from_object(__name__)
-app.config['FLASKY_MAIL_SUBJECT_PREFIX'] = '[FLASKY]'
-app.config['FLASKY_MAIL_SENDER'] = 'Flasky Admin <zjuguxi@gmail.com>'
 app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
+app.config['FLASKY_MAIL_SUBJECT_PREFIX'] = '[FLASKY]'
+app.config['FLASKY_MAIL_SENDER'] = 'Flasky Admin <zjuguxi@gmail.com>'
 app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
 app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
 app.config['FLASKY_ADMIN'] = os.environ.get('FLASKY_ADMIN')
 
 db = SQLAlchemy(app)
-
 manager = Manager(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 migrate = Migrate(app, db)
-manager.add_command('db', MigrateCommand)
 mail = Mail(app)
 
 def send_async_email(app, msg):
@@ -61,16 +57,17 @@ class Role(db.Model):
     name = db.Column(db.String(64), unique = True)
     def __repr__(self):
         return '<Role %r>' % self.name
-    users = db.relationship('User', backref = 'role')
 
 
 class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String(64), unique = True, index = True)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
     def __repr__(self):
         return '<User %r>' % self.username
-    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+    
 
 
 class NameForm(Form):
@@ -98,13 +95,14 @@ def index():
             db.session.add(user)
             session['known'] = False
             if app.config['FLASKY_ADMIN']:
-                send_email(app.config['FLASKY_ADMIN'], 'New User', 'mail/new_user', user = user)
+                send_email(app.config['FLASKY_ADMIN'], 'New User',
+                            'mail/new_user', user = user)
         else:
             session['known'] = True
         session['name'] = form.name.data
-        form.name.data = ''
         return redirect(url_for('index'))
-    return render_template('index.html', form=form, name=session.get('name'), known = session.get('known', False))
+    return render_template('index.html', form=form, name=session.get('name'),
+                            known = session.get('known', False))
 
 
 if __name__ == '__main__':
