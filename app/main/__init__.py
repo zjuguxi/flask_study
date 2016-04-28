7c9e24b783939738fac22a2d3f625729572f6aae
Alien <zjuguxi@gmail.com>
2016-04-28 14:58:33 +0800

    view.py/errors.py

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -2,4 +2,11 @@ from flask import Blueprint
 
 main = Blueprint('main', __name__)
 
+def create_app(config_name):
+
+    from main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    return app
+
 from . import views, errors
