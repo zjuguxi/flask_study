ded7e9fa85fbdaed85272210b479605dca5cc0cf
Alien <zjuguxi@gmail.com>
2016-04-28 16:18:43 +0800

    a lot

--- manage.py
+++ manage.py
@@ -1,6 +1,6 @@
-#！ /usr/bin/env python
+#! /usr/bin/env python
 import os
-from app imoprt create_app, db
+from app import create_app, db
 from app.models import User, Role
 from flask.ext.script import Manager, Shell
 from flask.ext.migrate import Migrate, MigrateCommand
@@ -14,11 +14,12 @@ def make_shell_context():
 manager.add_command('shell', Shell(make_context = make_shell_context))
 manager.add_command('db', MigrateCommand)
 
-if __name__ == '__main__':
-    manager.run
-
+@manager.command
 def test():
     '''Run the unit tests. '''
     import unittest
     tests = unittest.TestLoader().discover('tests')
     unittest.TextTestRunner(verbosity = 2).run(tests)
+
+if __name__ == '__main__':
+    manager.run()
