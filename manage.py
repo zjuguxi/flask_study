a4f14aac2dad701693497aed7f7bf64711631d9e
Alien <zjuguxi@gmail.com>
2016-04-28 16:08:44 +0800

    edited unittest

--- manage.py
+++ manage.py
@@ -15,4 +15,10 @@ manager.add_command('shell', Shell(make_context = make_shell_context))
 manager.add_command('db', MigrateCommand)
 
 if __name__ == '__main__':
-    manager.run()
+    manager.run
+
+def test():
+    '''Run the unit tests. '''
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity = 2).run(tests)
