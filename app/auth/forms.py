e40c4ed7dd932741c9abf91178bdc52b63589f5e
Alien <zjuguxi@gmail.com>
2016-05-06 17:43:50 +0800

    add validation error

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,7 +1,27 @@
 from flask.ext.wtf import Form
 from wtforms import StringFiled, PasswordField, BooleanField, SubmitField
-from wtforms.validators import Required, Email
+from wtforms.validators import Required, Email, Length(), Regexp, EqualTo
+from wtforms import ValidationError
+from ..models import User
 
+class RegistrationForm(Form):
+    email = StringFiled('Email', validators = [Required(), Length(1, 64), Email()])
+    username = StringFiled('Username', validators = [
+        Required(), Length(1, 64), Regexp('^[A-Za-z][A-Za-z0-9_.]*$', 0, 'Usernames must have only letters, '
+            'numbers, dots or underscores')])
+    password = PasswordField('Password', validators = [Required(), EqualTo('password2', message = 'password must match')])
+    password2 = PasswordField('Confirm password', validators = [Required()])
+    submit = SubmitField('Register')
+
+    def validate_email(self, field):
+        if User.query.filter_by(email = field.data).first():
+            raise ValidationError('Email already registerd.')
+
+    def validate_username(self, field):
+        if User.query.filter_by(username = field.data).first():
+            raise ValidationError('Username already in use.')
+
+            
 class LoginForm(Form):
     email = StringFiled('Email', validators = [Required(), Length(1, 64), Email()])
     password = PasswordField('Password', validators = [Required()])
