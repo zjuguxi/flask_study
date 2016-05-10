cfa9c09ef73a04950ca70f5331dc7a92efcd2211
Alien <zjuguxi@gmail.com>
2016-05-10 15:27:06 +0800

    add register.html

--- app/auth/views.py
+++ app/auth/views.py
@@ -22,3 +22,13 @@ def logout():
     flash('You have been logged out.')
     return redirect(url_for('main.index'))
 
+@auth.route('/register', methods = ['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email = form.email.data, username = form.username.data, password = form.password.data)
+        db.session.add(user)
+        flash('You can now login.')
+        return redirect(url_for('auth.login'))
+    return render_template('auth/register.html', form = form)
+
