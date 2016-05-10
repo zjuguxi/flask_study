c2288fd3020488ac4352e70caef29d6f95120ae2
Alien <zjuguxi@gmail.com>
2016-05-10 20:03:24 +0800

    finished

--- app/__init__.py
+++ app/__init__.py
@@ -3,8 +3,8 @@ from flask.ext.bootstrap import Bootstrap
 from flask.ext.mail import Mail
 from flask.ext.moment import Moment
 from flask.ext.sqlalchemy import SQLAlchemy
-from config import config
 from flask.ext.login import LoginManager
+from config import config
 
 bootstrap = Bootstrap()
 mail = Mail()
@@ -14,6 +14,7 @@ db = SQLAlchemy()
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
+
 
 def create_app(config_name):
     app = Flask(__name__)
@@ -29,9 +30,7 @@ def create_app(config_name):
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
-    return app
-
     from .auth import auth as auth_blueprint
-    app.register_blueprint(auth_blueprint, url_prefix = '/auth')
+    app.register_blueprint(auth_blueprint, url_prefix='/auth')
 
     return app
--- app/models.py
+++ app/models.py
@@ -1,9 +1,9 @@
 from werkzeug.security import generate_password_hash, check_password_hash
-from flask.ext.login import UserMixin
-from . import db, login_manager
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 from flask import current_app
-from . import db
+from flask.ext.login import UserMixin
+from . import db, login_manager
+
 
 class Role(db.Model):
     __tablename__ = 'roles'
@@ -24,22 +24,6 @@ class User(UserMixin, db.Model):
     password_hash = db.Column(db.String(128))
     confirmed = db.Column(db.Boolean, default=False)
 
-    def generate_confirmation_token(self, expiration=3600):
-        s = Serializer(current_app,.config['SECRET_KEY'], expiration)
-        return s.dumps({'confirm': self.id})
-
-    def comfirm(self, token):
-        s = Serializer(current_app.config['SECRET_KEY'])
-        try:
-            data = s.loads(tokens)
-        except:
-            return False
-        if data.get('confirm') != self.id
-            return False
-        self.confirmed = True
-        db.session.add(self)
-        return True
-
     @property
     def password(self):
         raise AttributeError('password is not a readable attribute')
@@ -50,6 +34,22 @@ class User(UserMixin, db.Model):
 
     def verify_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+    def generate_confirmation_token(self, expiration=3600):
+        s = Serializer(current_app.config['SECRET_KEY'], expiration)
+        return s.dumps({'confirm': self.id})
+
+    def confirm(self, token):
+        s = Serializer(current_app.config['SECRET_KEY'])
+        try:
+            data = s.loads(token)
+        except:
+            return False
+        if data.get('confirm') != self.id:
+            return False
+        self.confirmed = True
+        db.session.add(self)
+        return True
 
     def __repr__(self):
         return '<User %r>' % self.username
