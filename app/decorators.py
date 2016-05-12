557e5a512b0a4d71bb191478355d8558582d50d2
Alien <zjuguxi@gmail.com>
2016-05-12 16:05:10 +0800

    user page

--- app/decorators.py
+++ app/decorators.py
@@ -1,6 +1,8 @@
 from functools import wraps
 from flask import abort
 from flask.ext.login import current_user
+from .models import Permission
+
 
 def permission_required(permission):
     def decorator(f):
@@ -11,5 +13,7 @@ def permission_required(permission):
             return f(*args, **kwargs)
         return decorated_function
     return decorator
+
+
 def admin_required(f):
-    return permission_required(Permission.ADMINISTER)
+    return permission_required(Permission.ADMINISTER)(f)
