13bad0863afe797bb3179a0a80c5fcb88b67d2ae
Alien <zjuguxi@gmail.com>
2016-05-12 16:24:16 +0800

    edit profile

--- app/main/views.py
+++ app/main/views.py
@@ -12,3 +12,19 @@ def index():
 def user(username):
     user = User.query.filter_by(username=username).first_or_404()
     return render_template('user.html', user=user)
+
+@main.route('/edit-profile', methods = ['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.name = form.name.data
+        current_user.location = form.location.data
+        current_user.about_me = form.about_me.data
+        db.session.add(user)
+        flash('Your profile has been updated.')
+        return redirect(url_for('.user', username = current_user.username))
+    form.name.data = current_user.name
+    form.location.data = current_user.location
+    form.about_me.data = current_user.about_me
+    return render_template('edit_profile.html', form = form)
