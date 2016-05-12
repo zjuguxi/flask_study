d269e95b203e770745eb6d8f4feb7fecd6cbfb3c
Alien <zjuguxi@gmail.com>
2016-05-12 14:53:30 +0800

    role assignment

--- app/models.py
+++ app/models.py
@@ -1,7 +1,7 @@
 from werkzeug.security import generate_password_hash, check_password_hash
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 from flask import current_app
-from flask.ext.login import UserMixin
+from flask.ext.login import UserMixin, AnonymousUserMixin
 from . import db, login_manager
 
 
@@ -31,7 +31,7 @@ class Role(db.Model):
             role = Role.query.filter_by(name = r).first()
             if role is None:
                 role = Role(name = r)
-            role.permission = roles[r][0]
+            role.permissions = roles[r][0]
             role.default = roles[r][1]
             db.session.add(role)
         db.session.commit()
@@ -52,6 +52,26 @@ class User(UserMixin, db.Model):
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
     password_hash = db.Column(db.String(128))
     confirmed = db.Column(db.Boolean, default=False)
+
+    def __init__(self, **kwargs):
+        super(User, self).__init__(**kwargs)
+        if self.role is None:
+            if self.email == current_app.config['FLASKY_ADMIN']:
+                self.role = Role.query.filter_by(permissions = 0xff).first()
+            if self.role is None:
+                self.role = Role.query.filter_by(default = True).first()
+    def can(self, permissions):
+        return self.role is not None and \
+            (self.role.permissions & permissions) == permissions
+    def is_administrator(self):
+        return self.can(Permission.ADMINISTER)
+
+class AnonymousUser(AnonymousUserMixin):
+    def can(self, permissions):
+        return False
+    def is_administrator(self):
+        return False
+login_manager.anonymous_user = AnonymousUser
 
     @property
     def password(self):
