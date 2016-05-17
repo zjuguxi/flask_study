41b68fe0acdb3f44dd9475ac54e905201baf3fa0
Alien <zjuguxi@gmail.com>
2016-05-17 16:14:32 +0800

    forgerypy

--- app/models.py
+++ app/models.py
@@ -12,6 +12,21 @@ class Post(db.Model):
     body = db.Column(db.Text)
     tiemstamp = db.Column(db.DateTime, index = True, default = datetime.utcnow)
     author_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    
+    @staticmethod
+    def generate_fake(count = 100):
+        from random import seed, randint
+        import forgery_py
+
+        seed()
+        user_count = User.query.count()
+        for i in range(count):
+            u = User.query.offset(randint(0, user_count - 1)).first()
+            p = Post(body = forgery_py.lorem_ipsum.sentence(radint(1, 3)),
+                            tiemstamp = forgery_py.date.date(True),
+                            author = u)
+            db.session.add(p)
+            db.session.commit()
 
 class Permission:
     FOLLOW = 0x01
@@ -29,7 +44,6 @@ class Role(db.Model):
     permissions = db.Column(db.Integer)
     users = db.relationship('User', backref='role', lazy='dynamic')
 
-    @staticmethod
     def insert_roles():
         roles = {
             'User': (Permission.FOLLOW |
@@ -69,6 +83,28 @@ class User(UserMixin, db.Model):
     last_seen = db.Column(db.DateTime(), default=datetime.utcnow)
     avatar_hash = db.Column(db.String(32))
     posts = db.relationship('Post', backref = 'author', lazy = 'dynamic')
+
+    @staticmethod
+    def generate_fake(count=100):
+        from sqlalchemy.exc import IntegrityError
+        from random import seed
+        import forgery_py
+
+        seed()
+        for i in range(count):
+            u = User(email = forgery_py.internet.email_address(),
+                            username = forgery_py.internet.user_name(True),
+                            password = forgery_py.lorem_ipsum.word(),
+                            confirmed = True,
+                            name = forgery_py.name.full_name(),
+                            location = forgery_py.address.city(),
+                            about_me = forgery_py.lorem_ipsum.sentence(),
+                            member_since = forgery_py.date.date(True))
+            db.session.add(u)
+            try:
+                db.session.commit()
+            except:
+                db.session.rollback()
 
     def __init__(self, **kwargs):
         super(User, self).__init__(**kwargs)
