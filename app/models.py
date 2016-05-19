705da2a8dd51370a6b1959583e21869df5014787
Alien <zjuguxi@gmail.com>
2016-05-19 16:04:07 +0800

    add follow methods

--- app/models.py
+++ app/models.py
@@ -75,6 +75,25 @@ class User(UserMixin, db.Model):
                                                      lazy = 'dynamic',
                                                      cascade = 'all, delete-orphan')
 
+    def follow(self, user):
+        if not self.is_following(user):
+            f = Follow(follower = self, followed = user)
+            db.session.add(f)
+
+    def unfollow(self, user):
+        f = self.followed.filter_by(followed_id = user.id).first()
+        if f:
+            db.session.delete(f)
+
+    def is_following(self, user):
+        return self.followed.filter_by(
+            followed_id = user.id).first() is not None
+
+    def is_followed_by(self, user):
+        return self.followers.filter_by(
+            follower_id = user.id).first() is not None
+        
+
     @staticmethod
     def generate_fake(count=100):
         from sqlalchemy.exc import IntegrityError
