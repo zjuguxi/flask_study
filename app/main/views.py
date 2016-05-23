9e6346e147204d9a2cd7b2961daf4b1fc2bd5632
Alien <zjuguxi@gmail.com>
2016-05-23 16:56:32 +0800

    moderate comments

--- app/main/views.py
+++ app/main/views.py
@@ -203,10 +203,21 @@ def show_all():
     resp.set_cookie('show_followed', '', max_age=30*24*60*60)
     return resp
 
-
 @main.route('/followed')
 @login_required
 def show_followed():
     resp = make_response(redirect(url_for('.index')))
     resp.set_cookie('show_followed', '1', max_age=30*24*60*60)
     return resp
+
+@main.route('/moderate')
+@login_required
+@permission_required(Permission.MODERATE_COMMENTS)
+def moderate():
+    page = request.args.get('page', 1, type = int)
+    pagination = Comment.query.order_by(Comment.timestamp.desc()).paginate(
+        page, per_page = current_app.config['FLASKY_COMMENTS_PER_PAGE'],
+        error_out = False)
+    comments = pagination.items
+    return render_template('moderate.html', comments = comments, 
+                                         pagination = pagination, page = page)
