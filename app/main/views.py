6becb0d80999c10a713f48b2ea3c96a29e964eb4
Alien <zjuguxi@gmail.com>
2016-05-23 15:01:12 +0800

    debug

--- app/main/views.py
+++ app/main/views.py
@@ -1,5 +1,5 @@
 from flask import render_template, redirect, url_for, abort, flash, request,\
-    current_app
+    current_app, make_response
 from flask.ext.login import login_required, current_user
 from . import main
 from .forms import EditProfileForm, EditProfileAdminForm, PostForm
@@ -30,26 +30,12 @@ def index():
         query =current_user.followed_posts
     else:
         query = Post.query
-    pagination = query.order_by(Post.timestamp.desc()).pagination(page, 
+    pagination = query.order_by(Post.timestamp.desc()).paginate(page, 
         per_page = current_app.config['FLASKY_POSTS_PER_PAGE'],
         error_out = False)
     posts = pagination.items
     return render_template('index.html', form=form, posts=posts,
                            show_followde = show_followed, pagination=pagination)
-
-@main.route('/all')
-@login_required
-def show_all():
-    resp = make_response(redirect(url_for('.index')))
-    resp.set_cookie('show_followed', ' ', max_age = 30*24*60*60)
-    return resp
-
-@main.route('/floowed')
-@login_required
-def show_followed():
-    resp = make_response(redirect(url_for('.index')))
-    resp.set_cookie('show_followed', '1', max_age = 30*24*60*60)
-    return resp
 
 @main.route('/user/<username>')
 def user(username):
@@ -194,3 +180,17 @@ def followed_by(username):
     return render_template('followers.html', user=user, title="Followed by",
                            endpoint='.followed_by', pagination=pagination,
                            follows=follows)
+
+@main.route('/all')
+@login_required
+def show_all():
+    resp = make_response(redirect(url_for('.index')))
+    resp.set_cookie('show_followed', ' ', max_age = 30*24*60*60)
+    return resp
+
+@main.route('/followed')
+@login_required
+def show_followed():
+    resp = make_response(redirect(url_for('.index')))
+    resp.set_cookie('show_followed', '1', max_age = 30*24*60*60)
+    return resp
