5f02ae7e0b4d0be42729534dd2a86758ea3e5650
Alien <zjuguxi@gmail.com>
2016-05-23 14:38:41 +0800

    add

--- app/main/views.py
+++ app/main/views.py
@@ -37,6 +37,19 @@ def index():
     return render_template('index.html', form=form, posts=posts,
                            show_followde = show_followed, pagination=pagination)
 
+@main.route('/all')
+@login_required
+def show_all():
+    resp = make_response(redirect(url_for('.index')))
+    resp.set_cookie('show_followed', ' ', max_age = 30*24*60*60)
+    return resp
+
+@main.route('/floowed')
+@login_required
+def show_followed():
+    resp = make_response(redirect(url_for('.index')))
+    resp.set_cookie('show_followed', '1', max_age = 30*24*60*60)
+    return resp
 
 @main.route('/user/<username>')
 def user(username):
