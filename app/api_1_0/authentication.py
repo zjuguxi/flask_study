88eaec55a40c81f26c672c720e101336932c2d78
Alien <zjuguxi@gmail.com>
2016-05-24 16:22:18 +0800

    add

--- app/api_1_0/authentication.py
+++ app/api_1_0/authentication.py
@@ -1,5 +1,15 @@
 from flask.ext.httpauth import HTTPBasicAuth
+from .errors import forbidden_error
+
+
 auth = HTTPBasicAuth()
+
+@api.before_request
+@auth.login_required
+def before_request():
+    if not g.current_user.is_anonymous and \
+                not g.current_user.confirmed:
+        return fobidden('Unconfirmed account')
 
 @auth.verify_password
 def verify_password(email, password):
@@ -12,3 +22,12 @@ def verify_password(email, password):
         return False
     g.current_user = user
     return user.verify_password(password)
+
+@auth.error_handler
+def auth_error():
+    return unauthorized('Invalid credentials')
+
+@api.route('/posts/')
+@auth.login_required
+def get_posts():
+    pass
