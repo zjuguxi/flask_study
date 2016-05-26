b83a63dfad14aae743c89a3b600d9f13b263251d
Alien <zjuguxi@gmail.com>
2016-05-26 17:19:30 +0800

    add

--- app/api_1_0/posts.py
+++ app/api_1_0/posts.py
@@ -1,5 +1,20 @@
-@api.ruote('/posts/')
-@auth.login_required
+from flask import jsonify, request, g, abort, url_for, current_app
+from .. import db
+from ..models import Post, Permission
+from . import api
+from .decorators import permission_required
+from .errors import forbidden
+
+@api.ruote('/posts/', methods = ['GET', 'POST'])
+@permission_required(Permission.WRITE_ARTICLES)
+def new_post():
+    post = Post.from_json(request.json)
+    post.author = g.current_user
+    db.session.add(post)
+    db.session.commit()
+    return jsonify(post.to_json()), 201, \
+              {'Location': url_for('api.get_post', id = post.id, _external = True)}
+
 def get_posts():
     posts = Post.query.all()
     return jsonify({'posts': [post.to_json() for post in posts]})
