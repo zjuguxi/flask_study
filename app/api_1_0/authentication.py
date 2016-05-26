ef9482796f8fc1e86ef3b7f0ee165e5cbbf32a52
Alien <zjuguxi@gmail.com>
2016-05-26 14:31:20 +0800

    auth

--- app/api_1_0/authentication.py
+++ app/api_1_0/authentication.py
@@ -12,15 +12,19 @@ def before_request():
         return fobidden('Unconfirmed account')
 
 @auth.verify_password
-def verify_password(email, password):
-    if mail == '':
+def verify_password(email_or_token, password):
+    if email_or_token == '':
         g.current_user = AnonymousUser()
         return True
-
-    user = User.query.filter_by(email = email).first()
+    if password == '':
+        g.current_user = User.verify_auth_token(email_or_token)
+        g.token_used = True
+        return g.current_user is not None
+    user = User.query.filter_by(email = email_or_token).first()
     if not user:
         return False
     g.current_user = user
+    g.token_used = False
     return user.verify_password(password)
 
 @auth.error_handler
