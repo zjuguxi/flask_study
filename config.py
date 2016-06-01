d6f9ba523aaf77b34be414d5416c4db52c6f4afe
Alien <zjuguxi@gmail.com>
2016-06-01 15:01:56 +0800

    add config timeout

--- config.py
+++ config.py
@@ -16,6 +16,8 @@ class Config:
     FLASKY_POSTS_PER_PAGE = 20
     FLASKY_FOLLOWERS_PER_PAGE = 50
     FLASKY_COMMENTS_PER_PAGE = 30
+    SQLALCHEMY_RECORD_QUERIES = True
+    FLASKY_DB_TIMEOUT = 0.5
 
     @staticmethod
     def init_app(app):
