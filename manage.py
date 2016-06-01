c6eaca4f0ea89ffee8f975437b06d54d33873798
Alien <zjuguxi@gmail.com>
2016-06-01 15:24:21 +0800

    profiler

--- manage.py
+++ manage.py
@@ -44,8 +44,8 @@ def test(coverage=False):
         print('HTML version: file://%s/index.html' % covdir)
         COV.erase()
 
-@manage.command
-def profile(length = 25, profile_dir = None)
+@manager.command
+def profile(length = 25, profile_dir = None):
     """Start the application under the code profiler."""
     from werkzeug.contrib.profiler import ProfilerMiddleware
     app.wsgi_app = ProfilerMiddleware(app.wsgi_app, restrictions = [length], profile_dir = profile_dir)
